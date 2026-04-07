586ff262dd95cd4d1945f13f78135d8c89fd905c
Sergio Pineda <sergio.pineda@alu.ceacfp.es>
2026-04-07 10:14:41 +0200

    fase c

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,17 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', taskRoutes)
  
+// Middleware global de errores — siempre al final
+app.use((err, req, res, next) => {
+  if (err.message === 'NOT_FOUND') {
+    return res.status(404).json({ error: 'Recurso no encontrado' });
+  }
+
+  // Para cualquier otro error no controlado
+  console.error(err);
+  res.status(500).json({ error: 'Error interno del servidor' });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
