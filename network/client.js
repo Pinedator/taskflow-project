d0410b5934b58f0f5332ce7988f0f3b20957d292
Sergio Pineda <sergio.pineda@alu.ceacfp.es>
2026-04-09 14:19:42 +0200

    vercel fix

--- network/client.js
+++ network/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/tasks';
+const API_URL = 'https://taskflow-server.vercel.app/api/v1/tasks';
 
 export const obtenerTareas = async () => {
   const res = await fetch(API_URL);
